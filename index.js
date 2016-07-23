a421142e79782f6d78dc88d02856ad3b128442a9
Fred Prieur <fprieur@gmail.com>
2016-07-22 22:44:50 -0400

    make create argument mandatory

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 'use strict';
 const program = require('commander');
-const file = require('./file');
+var file = require('./file');
 
 program
   .version('0.0.1')
-  .command("create [name]")
+  .command("create <name>")
   .description('Generate a quickstart project')
   .action(function(name){
     console.log('create a project named "%s"', name);
