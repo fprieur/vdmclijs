d9947697d186435672f890de2023b499b80c2bbb
Fred Prieur <fprieur@gmail.com>
2016-07-27 13:13:45 -0400

    we can now create Dockerfile and Jenkinsfile with the content of a remote git repo

--- file.js
+++ file.js
@@ -1,11 +1,33 @@
 var writeFile = require('write');
+var request = require('request');
+var config = require('config');
+var workdir = config.get('Workdir.dev');
 
 function CreateDockerfile(){
-  writeFile('src/Dockerfile',
-            "FROM node:4.3 \nEXPOSE 3000",
-            function(err) {
-              if (err) console.log(err);
+  request.get('https://raw.githubusercontent.com/fprieur/test-pipeline-jenkins/master/Dockerfile', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var content = body;
+        writeFile(workdir+'src/Dockerfile',
+                  content,
+                  function(err) {
+                    if (err) console.log(err);
+        });
+    }
+  });
+}
+
+function CreateJenkinsfile(){
+  request.get('https://raw.githubusercontent.com/fprieur/test-pipeline-jenkins/master/Jenkinsfile', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var content = body;
+        writeFile(workdir+'src/Jenkinsfile',
+                  content,
+                  function(err) {
+                    if (err) console.log(err);
+        });
+    }
   });
 }
 
 exports.CreateDockerfile = CreateDockerfile;
+exports.CreateJenkinsfile = CreateJenkinsfile;
--- index.js
+++ index.js
@@ -6,11 +6,12 @@ var github = require ('./github');
 
 program
   .version('0.0.1')
-  .command("create <name>")
+  .command("create_project <name>")
   .description('Generate a quickstart project')
   .action(function(name){
     console.log('create a project named "%s"', name);
     file.CreateDockerfile();
+    file.CreateJenkinsfile();
   });
 
 program
