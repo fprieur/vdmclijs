94bf9426110ce6a79e6bb39510a00c64310e6c96
Fred Prieur <fprieur@gmail.com>
2016-07-27 12:30:03 -0400

    we can now create public repo on github, add config module

---
+++ github.js
@@ -0,0 +1,32 @@
+var GitHubApi = require("github");
+var config = require('config');
+
+var github = new GitHubApi({
+    // optional
+    debug: true,
+    protocol: "https",
+    host: "api.github.com", // should be api.github.com for GitHub
+    headers: {
+        "user-agent": "vdmcli" // GitHub is happy with a unique user agent
+    },
+    Promise: require('bluebird'),
+    followRedirects: true, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
+    timeout: 5000
+});
+
+function CreateRepo(name) {
+  var githubToken = config.get('Github.token');
+
+  github.authenticate({
+    type: "oauth",
+    token: githubToken
+  });
+
+  github.repos.create({
+    name: name
+  }, function(err, res) {
+      console.log(JSON.stringify(res));
+  });
+}
+
+exports.CreateRepo = CreateRepo;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 'use strict';
 const program = require('commander');
 var file = require('./file');
+var github = require ('./github');
 
 program
   .version('0.0.1')
@@ -10,6 +11,14 @@ program
   .action(function(name){
     console.log('create a project named "%s"', name);
     file.CreateDockerfile();
+  });
+
+program
+  .command("create_repo <repo_name>")
+  .description('Create de public repo on github with the given name')
+  .action(function(name){
+    console.log('Create the repo  "%s"', name);
+    github.CreateRepo(name);
   });
 
 program.parse(process.argv);
