3c59670856deeaedd0f6adc7795a18bd7de9d797
fprieur <fprieur@gmail.com>
2016-07-29 11:56:37 -0400

    add todo comments in file.js

--- file.js
+++ file.js
@@ -2,7 +2,7 @@ var writeFile = require('write');
 var request = require('request');
 var config = require('config');
 var workdir = config.get("workdir.dev");
-
+// TODO: remove url to base dockerfile and put in config
 function CreateDockerfile(){
   request.get('https://raw.githubusercontent.com/fprieur/test-pipeline-jenkins/master/Dockerfile', function (error, response, body) {
     if (!error && response.statusCode == 200) {
@@ -15,7 +15,7 @@ function CreateDockerfile(){
     }
   });
 }
-
+// TODO: remove url to base Jenkinsfile and put in config
 function CreateJenkinsfile(){
   request.get('https://raw.githubusercontent.com/fprieur/test-pipeline-jenkins/master/Jenkinsfile', function (error, response, body) {
     if (!error && response.statusCode == 200) {
